960097f807f6cabb3c476f1869685729415e4d1c
duresapz <pajazitiduresa@hotmail.com>
2020-03-12 21:55:40 +0100

    Nutriments JSON

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,9 +1,8 @@
 class Burger < ApplicationRecord
     def nutriments
-        require 'openfoodfacts'
-        @product=openfoodfacts::Product.get(self.code, locale: 'fr')
+        @product=Openfoodfacts::Product.get(self.code, locale: 'fr')
         if @product == nil
-            @json = {"No Data"=> "No code"}
+            @json = {"" => ""}
         else
             @json = @product.nutriments.to_hash
         end
